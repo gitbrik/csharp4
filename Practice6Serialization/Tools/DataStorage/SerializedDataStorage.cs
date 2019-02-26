67c4f48235003beb734a30a666c80dc36667bba7
Volodymyr Yablonskyi <yablonskyi.v.reg@gmail.com>
2019-02-26 22:34:46 +0200

    Practice6

---
+++ Practice6Serialization/Tools/DataStorage/SerializedDataStorage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using KMA.ProgrammingInCSharp2019.Practice6.Serialization.Models;
+using KMA.ProgrammingInCSharp2019.Practice6.Serialization.Tools.Managers;
+
+namespace KMA.ProgrammingInCSharp2019.Practice6.Serialization.Tools.DataStorage
+{
+    internal class SerializedDataStorage:IDataStorage
+    {
+        private readonly List<User> _users;
+
+        internal SerializedDataStorage()
+        {
+            try
+            {
+                _users = SerializationManager.Deserialize<List<User>>(FileFolderHelper.StorageFilePath);
+            }
+            catch (FileNotFoundException)
+            {
+                _users = new List<User>();
+            }
+        }
+        
+        public bool UserExists(string login)
+        {
+            return _users.Exists(u => u.Login == login);
+        }
+
+        public User GetUserByLogin(string login)
+        {
+            return _users.FirstOrDefault(u => u.Login == login);
+        }
+
+        public void AddUser(User user)
+        {
+            _users.Add(user);
+            SaveChanges();
+        }
+
+        private void SaveChanges()
+        {
+            SerializationManager.Serialize(_users, FileFolderHelper.StorageFilePath);
+        }
+        
+    }
+}
+
---
+++ Practice6Serialization/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using KMA.ProgrammingInCSharp2019.Practice6.Serialization.Tools;
+using KMA.ProgrammingInCSharp2019.Practice6.Serialization.Tools.Managers;
+
+namespace KMA.ProgrammingInCSharp2019.Practice6.Serialization.ViewModels
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        public string CurrentUser
+        {
+            get
+            {
+                return $"Current User {StationManager.CurrentUser}";
+            }
+        }
+        
+    }
+}
